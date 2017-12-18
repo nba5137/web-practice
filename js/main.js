f3eed6e5f19b29d247b1e32d498247e2a5ef59b1
nba5137 <641033735@qq.com>
2017-12-17 21:53:12 -0700

    Added a new font. Improved function for 'About me'.

--- js/main.js
+++ js/main.js
@@ -10,6 +10,12 @@
 
 /* 1. Scroll to top of the page */
 function Func_top() {
-    $('html, body').animate({scrollTop:0}, 1300);
+    $('html, body').animate({scrollTop:0}, 1500);
+}
+
+function Func_me() {
+    $('html, body').animate({
+        scrollTop: $("#me").offset().top
+    }, 1500);
 }
 
